7fec8935fed9a67da5804bc5d7dd4fd50410bffe
James Bergstra <james.bergstra@gmail.com>
2012-06-30 20:36:54 -0400

    unsup missing files

---
+++ py_unsup/q_dae.py
@@ -0,0 +1,8 @@
+
+# What happens to the filter-learning process when the noise-free autoencoder
+# is subjected to noise at various points in the algorithm calculation. All of
+# these might be justifiable in some sense as making the internal
+# representations more robust, why is applying noise to the input best? Is it?
+#
+#
+
